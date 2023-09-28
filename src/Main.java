ac89ef4c30b7c9931408d66850c1f68fe1dfa529
Lukebalek <Lukebalek23@gmail.com>
2023-09-28 15:26:56 -0400

    Party

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Display the menu
         System.out.println("Select your party affiliation:");
         System.out.println("D - Democrat");
         System.out.println("R - Republican");
         System.out.println("I - Independent");
         System.out.print("Enter your choice: ");
 
-        // Read the user's choice
         char choice = scanner.next().charAt(0);
 
-        // Determine the response based on the user's choice
         switch (choice) {
             case 'D':
             case 'd':
