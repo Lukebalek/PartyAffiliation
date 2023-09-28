60ab51fa4ade95f4e0825503f65a2cebb2ff3a17
Lukebalek <Lukebalek23@gmail.com>
2023-09-28 16:32:34 -0400

    Party

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Select your party affiliation:");
+        System.out.println("Choose your party affiliation:");
         System.out.println("D - Democrat");
         System.out.println("R - Republican");
         System.out.println("I - Independent");
         System.out.print("Enter your choice: ");
 
-        char choice = scanner.next().charAt(0);
+        String choice = scanner.next();
 
-        switch (choice) {
-            case 'D':
-            case 'd':
-                System.out.println("You get a Democratic Donkey.");
-                break;
-            case 'R':
-            case 'r':
-                System.out.println("You get a Republican Elephant.");
-                break;
-            case 'I':
-            case 'i':
-                System.out.println("You get an Independent Person.");
-                break;
-            default:
-                System.out.println("You get an Other.");
+        String response;
+
+        if ("D".equalsIgnoreCase(choice)) {
+            response = "Democratic Donkey";
+        } else if ("R".equalsIgnoreCase(choice)) {
+            response = "Republican Elephant";
+        } else if ("I".equalsIgnoreCase(choice)) {
+            response = "Independent Person";
+        } else {
+            response = "That is an invalid command, please use [R,D,I] ";
         }
 
+        System.out.println("You get a " + response + ".");
+
         scanner.close();
+
     }
 }
